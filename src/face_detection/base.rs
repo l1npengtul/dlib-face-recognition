997abf83041d3e20cc02a84398e76a66bbd146e7
l1npengtul <l1npengtul@protonmail.com>
2021-03-16 21:23:37 +0900

    send sync impl

--- src/face_detection/base.rs
+++ src/face_detection/base.rs
@@ -1,7 +1,7 @@
 use super::location::FaceLocations;
 use crate::matrix::ImageMatrix;
 
-pub trait FaceDetectorTrait {
+pub trait FaceDetectorTrait: Send + Sync {
     /// Detect face rectangles from an image.
     fn face_locations(&self, image: &ImageMatrix) -> FaceLocations;
 }
